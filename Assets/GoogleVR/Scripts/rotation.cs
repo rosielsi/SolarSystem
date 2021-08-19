5ea9622648f7bca69031e76d4db03676a7acecfe
rosielsi <87419744+rosielsi@users.noreply.github.com>
2021-08-19 20:58:16 +0800

    deleteme new script

---
+++ Assets/GoogleVR/Scripts/rotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform trans_target;
+    public int speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(trans_target.transform.position,trans_target.transform.up,speed*Time.deltaTime);
+    }
+}
