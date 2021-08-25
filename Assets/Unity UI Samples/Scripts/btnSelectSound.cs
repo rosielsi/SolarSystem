ca80b8826ddf122eb34b519bc6cf70e1758ace56
rosielsi <87419744+rosielsi@users.noreply.github.com>
2021-08-25 14:12:56 +0800

    sfxbutton new script

---
+++ Assets/Unity UI Samples/Scripts/btnSelectSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnSelectSound : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFx);
+    }
+}
